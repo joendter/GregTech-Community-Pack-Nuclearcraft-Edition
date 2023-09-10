544bf74bc5b583bdc687c9874f421110bc95e2c1
joendter <71767955+joendter@users.noreply.github.com>
2023-09-10 12:20:43 +0200

    fix materials.groovy

--- groovy/preInit/materials.groovy
+++ groovy/preInit/materials.groovy
@@ -1,5 +1,4 @@
 import gregtech.api.unification.material.properties.PropertyKey
-import gregtech.api.GregTechAPI.MaterialEvent
 import gregtech.api.unification.material.Material
 import gregtech.api.unification.material.properties.IngotProperty
 import gregtech.api.unification.material.properties.FluidProperty
@@ -11,11 +10,8 @@ import net.minecraft.util.ResourceLocation
 
 import static gregtech.api.unification.material.info.MaterialFlags.*
 
-
-// register an event listener
 event_manager.listen { MaterialEvent event ->
-    // create materials here
-new Material.Builder(32000, resource('gcp', 'fluix'))
+    new Material.Builder(32000, resource('gcp', 'fluix'))
             .gem()
             .color(0x674FAF).iconSet('CERTUS')
             .flags('generate_plate', 'disable_decomposition', 'no_smelting', 'crystallizable')
@@ -26,56 +22,66 @@ new Material.Builder(32000, resource('gcp', 'fluix'))
     material('nether_quartz').addFlags('generate_rod', 'generate_bolt_screw')
     material('iron').addFlags('generate_dense')
     material('brass').addFlags('generate_spring')
-//fluorite https://en.wikipedia.org/wiki/Fluorite
-def fluorite = new Material.Builder(32006, "fluorite")
-    .fluid()
-    .gem()
-    .color(0x80A787)
-    .ore(2, 1, true)
-    .addOreByproducts(material('nether_quartz'), material('calcite'))
-    .fluidTemp(1800)
-    .iconSet("QUARTZ")
-    .components(material("calcium"),1,material("fluorine"),2)
-    .flags("decomposition_by_electrolyzing", "no_smelting", "crystallizable", "high_sifter_output")
-    .build()
 
-def villiaumite = new Material.Builder(32007, "villiaumite")
-    .fluid()
-    .gem()
-    .color(0x560201)
-    .ore(2, 1, true)
-    .addOreByproducts(material('sodalite'), material('zeolite'))
-    .fluidTemp(1400)
-    .iconSet("QUARTZ")
-    .components(material("sodium"),1,material("fluorine"),1)
-    .flags("decomposition_by_electrolyzing", "no_smelting", "crystallizable", "high_sifter_output")
-    .build()
+    new Material.Builder(32006, resource('gcp',"fluorite"))
+        .fluid()
+        .gem()
+        .color(0x80A787)
+        .ore(2, 1, true)
+        .addOreByproducts(material('nether_quartz'), material('calcite'))
+        .fluidTemp(1800)
+        .iconSet("QUARTZ")
+        .components(material("calcium"), 1, material("fluorine"), 2)
+        .flags("decomposition_by_electrolyzing", "no_smelting", "crystallizable", "high_sifter_output")
+        .build()
 
-def carobbiite = new Material.Builder(32008, "carobbiite")
-    .fluid()
-    .gem()
-    .color(0xE0F0D5)
-    .ore(2, 1, true)
-    .addOreByproducts(material('salt'), material('fluorite'))
-    .fluidTemp(1300)
-    .iconSet("QUARTZ")
-    .components(material("potassium"),1,material("fluorine"),1)
-    .flags("decomposition_by_electrolyzing", "no_smelting", "crystallizable", "high_sifter_output")
-    .build()
+    new Material.Builder(32007, resource('gcp',"villiaumite"))
+        .fluid()
+        .gem()
+        .color(0x560201)
+        .ore(2, 1, true)
+        .addOreByproducts(material('sodalite'), material('zeolite'))
+        .fluidTemp(1400)
+        .iconSet("QUARTZ")
+        .components(material("sodium"),1,material("fluorine"),1)
+        .flags("decomposition_by_electrolyzing", "no_smelting", "crystallizable", "high_sifter_output")
+        .build()
 
-def HSLA = new Material.Builder(32009, "h_s_l_a_steel")
-    .ingot()
-    .color(0x5d5f84)
-    .components(material("iron"),30, material("carbon"),1, material("manganese"), 1)
-    .blastTemp(1800)
-    .build()
+    new Material.Builder(32008, resource('gcp',"carobbiite"))
+        .fluid()
+        .gem()
+        .color(0xE0F0D5)
+        .ore(2, 1, true)
+        .addOreByproducts(material('salt'), material('fluorite'))
+        .fluidTemp(1300)
+        .iconSet("QUARTZ")
+        .components(material("potassium"),1,material("fluorine"),1)
+        .flags("decomposition_by_electrolyzing", "no_smelting", "crystallizable", "high_sifter_output")
+        .build()
 
-// def LqN = new Material.Builder(32010, "liquid_nitrogen")
-//     .fluid()
-//     .fluidTemp(70)
-//     .color(0x00b0b1)
-//     .components(material("nitrogen"),1)
-//     .build()
+    new Material.Builder(32009, resource('gcp',"h_s_l_a_steel"))
+        .ingot()
+        .color(0x5d5f84)
+        .components(material("iron"), 30, material("carbon"), 1, material("manganese"), 1)
+        .blastTemp(1800)
+        .build()
+
+    // tough alloy
+    new Material.Builder(32005, resource("gcp", "tough"))
+        .ingot()
+        .color(0x191323)
+        .iconSet("METALLIC")
+        .flags("disable_decomposition", "generate_plate", "generate_rod", "generate_frame")
+        .components(material("stainless_steel"), 1, material("titanium"), 1)
+        .blastTemp(1942)
+        .build()
+
+// // def LqN = new Material.Builder(32010, "liquid_nitrogen")
+// //     .fluid()
+// //     .fluidTemp(70)
+// //     .color(0x00b0b1)
+// //     .components(material("nitrogen"),1)
+// //     .build()
 
 String[] enrichments = [
     "slightly",
@@ -84,7 +90,7 @@ String[] enrichments = [
 ]
 
 for(int i = 0; i < enrichments.size(); i++){
-    new Material.Builder(32010+i, enrichments[i]+ "_enriched_uranium_hexafluoride")
+    new Material.Builder(32010+i, resource('gcp',enrichments[i]+ "_enriched_uranium_hexafluoride"))
         .fluid(FluidTypes.GAS)
         .color(0x42D126 + 0x020901 * (i+1))
         .flags("disable_decomposition")
@@ -106,5 +112,6 @@ byproducts = prop.getOreByProducts()
 byproducts.clear()
 byproducts.add(material("zirconium"))
 byproducts.add(material("rutile"))
+
 
 }
